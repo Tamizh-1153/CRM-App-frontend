94f80b0bf28d9909a4fbf08e929bb5732ab7bd63
Tamizh-1153 <tamizh1153@gmail.com>
2023-06-19 21:54:53 +0530

    frontend

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Login from "./Pages/Login"
+import Register from "./Pages/Register"
+import Dashboard from "./Pages/Dashboard"
+import Home from "./Pages/Home"
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" Component={Home} />
+          <Route path="/login" Component={Login} />
+          <Route path="/register" Component={Register} />
+          <Route path='/dashboard' Component={Dashboard} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Login.js
@@ -0,0 +1,61 @@
+import { useState } from "react"
+
+function App() {
+  
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const response = await fetch(
+      "https://crm-backend-frz0.onrender.com/api/v1/auth/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      }
+    )
+
+    const data = await response.json()
+    console.log(data.token)
+    localStorage.setItem("token", data.token)
+    if(data.user){
+      alert('Login successful')
+    }else{
+      alert('Login failed')
+    }
+    window.location.href = "http://localhost:3000/dashboard"
+  }
+
+  return (
+    <div>
+      <h1 className="text-center my-5">Login</h1>
+      <form onSubmit={handleSubmit} className="d-flex flex-column">
+        <input
+          className="form-control"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <br />
+        <input
+          className="form-control"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <br />
+        <input className="btn btn-primary my-3 " type="submit" value="Login" />
+      </form>
+    </div>
+  )
+}
+
+export default App
