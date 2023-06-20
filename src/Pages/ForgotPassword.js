eff697eb356f050ee6848b7b35ebe852409986a5
Tamizh-1153 <tamizh1153@gmail.com>
2023-06-20 09:15:28 +0530

    edit

---
+++ src/Pages/ForgotPassword.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
+const ForgotPassword = () => {
+  const [email, setEmail] = useState("")
+  const refresh=useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const response = await fetch(
+      "https://crm-backend-frz0.onrender.com/api/v1/auth/forgotPassword",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+        }),
+      }
+    )
+
+    const data = await response.json()
+    console.log(data)
+      if (data.status=='ok') {
+        alert("Sent password reset link")
+      } else {
+        alert("Email does not exists")
+      }
+      refresh("/login")
+  }
+
+  return (
+    <div>
+      <h1 className="text-center my-5">Forgot Password</h1>
+      <form onSubmit={handleSubmit} className="d-flex flex-column">
+        <input
+          className="form-control"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <input
+          className="btn btn-primary my-5 "
+          type="submit"
+          value="Send reset link"
+        />
+      </form>
+    </div>
+  )
+}
+
+export default ForgotPassword
---
+++ src/Pages/Login.js
@@ -0,0 +1,68 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
+
+function App() {
+  
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const refresh = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const response = await fetch(
+      "https://crm-backend-frz0.onrender.com/api/v1/auth/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      }
+    )
+
+    const data = await response.json()
+    console.log(data.token)
+    localStorage.setItem("token", data.token)
+    if(data.user){
+      alert('Login successful')
+    }else{
+      alert('Login failed')
+    }
+    refresh('/dashboard')
+    //window.location.href = "/dashboard"
+  }
+
+  return (
+    <div>
+      <h1 className="text-center my-5">Login</h1>
+      <form onSubmit={handleSubmit} className="d-flex flex-column">
+        <input
+          className="form-control"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <br />
+        <input
+          className="form-control"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <br />
+        <Link to='/forgotPassword'>
+        <input className="btn btn-primary my-3 " type="submit" value="Forgot Password" />
+        </Link>
+        <input className="btn btn-primary my-3 " type="submit" value="Login" />
+      </form>
+    </div>
+  )
+}
+
+export default App
